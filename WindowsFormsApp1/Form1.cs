78633622f616b9f303ec205934db2428321614f6
2512269324@qq.com <2512269324@qq.com>
2018-12-20 14:05:09 +0800

    vs项目

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string address = gameAddress.Text;
+            Process.Start(address);
+        }
+    }
+}
